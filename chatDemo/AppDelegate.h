f67f279b0578c56f5083331d04ea25c6d6f7f920
lizhenZuo <1732096868@qq.com>
2016-03-21 09:51:51 +0800

    Initial Commit

---
+++ chatDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chatDemo
+//
+//  Created by zorro on 16/3/21.
+//  Copyright © 2016年 zorro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chatDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chatDemo
+//
+//  Created by zorro on 16/3/21.
+//  Copyright © 2016年 zorro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
